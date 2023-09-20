51925ba1eb1e097a778b51fa71f182f14bdec6de
nahid12 <nahidraj.web@gmail.com>
2023-09-20 14:55:10 +0600

    first upload

---
+++ js/script.js
@@ -0,0 +1,74 @@
+$(function () {
+  "use strict";
+
+  // fixed menu js
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $('.sticky-top').addClass('shadow-lg').css('top', '0px');
+    } else {
+      $('.sticky-top').removeClass('shadow-lg').css('top', '-100px');
+    }
+  });
+
+  // mobile menu js
+  $('.mobile-topbar .icon i').click(function () {
+    $('.mobile-menu-main').addClass('show-mobile-menu')
+  })
+  $('.close-m-menu').click(function () {
+    $('.mobile-menu-main').removeClass('show-mobile-menu')
+  })
+  $('.mobile-topbar .icon i').click(function () {
+    $('.mobile-menu-overlay').addClass('show-mobile-menu-overlay')
+  })
+  $('.mobile-topbar .icon i').click(function () {
+    $('.mobile-menu-overlay').addClass('show-mobile-menu-overlay')
+  })
+  $('.close-m-menu').click(function () {
+    $('.mobile-menu-overlay').removeClass('show-mobile-menu-overlay')
+  })
+  $('.mobile-menu-overlay').click(function () {
+    $('.mobile-menu-overlay').removeClass('show-mobile-menu-overlay')
+  })
+  $('.mobile-menu-overlay').click(function () {
+    $('.mobile-menu-main').removeClass('show-mobile-menu')
+  })
+
+  $('.sub-menu ul').hide();
+  $(".sub-menu a").click(function () {
+    $(this).parent(".sub-menu").children("ul").slideToggle("100");
+    $(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
+  });
+
+
+  // back to top js
+  let btn = $('#button');
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+
+  btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: 0
+    }, '300');
+  });
+
+  // venobox js
+  new VenoBox({
+    selector: '.my-image-links',
+    numeration: true,
+    infinigall: true,
+    share: true,
+    spinner: 'rotating-plane'
+  });
+
+  new VenoBox({
+    selector: '.my-video-links',
+  });
+
+})
